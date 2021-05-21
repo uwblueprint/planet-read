d9e8bc2d4f76851a1e93d896f88e863d044a97e6
Jennifer Chen <jenn.chenn93@gmail.com>
2021-05-20 21:31:38 -0400

    feat(files): create file model

---
+++ backend/python/app/models/file.py
@@ -0,0 +1,24 @@
+from sqlalchemy import inspect
+from sqlalchemy.orm.properties import ColumnProperty
+
+from . import db
+
+
+class File(db.Model):
+    __tablename__ = "files"
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    link = db.Column(db.String(255), nullable=False)
+
+    def to_dict(self, include_relationships=False):
+        cls = type(self)
+        mapper = inspect(cls)
+        formatted = {}
+        for column in mapper.attrs:
+            field = column.key
+            attr = getattr(self, field)
+            if isinstance(column, ColumnProperty):
+                formatted[field] = attr
+            elif include_relationships:
+                formatted[field] = [obj.to_dict() for obj in attr]
+        return formatted
