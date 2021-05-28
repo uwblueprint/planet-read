6169d26b3d33d3d833088e225494b11d9c9e870f
Jennifer Chen <jenn.chenn93@gmail.com>
2021-05-27 22:39:14 -0400

    feat(users): add get_user by id/email to resolver

--- backend/python/app/graphql/queries/user_query.py
+++ backend/python/app/graphql/queries/user_query.py
@@ -1,11 +1,21 @@
 from ..service import services
 
-def resolve_users(root, info, **kwargs):
-    return services['user'].get_users()
 
-'''
+def resolve_users(root, info, **kwargs):
+    return services["user"].get_users()
+
+
+def resolve_user_by_id(root, info, id):
+    return services["user"].get_user_by_id(id)
+
+
+def resolve_user_by_email(root, info, email):
+    return services["user"].get_user_by_email(email)
+
+
+"""
 Required queries:
  userById(id: ID!): UserDTO!
  userByEmail(email: String!): UserDTO!
  users: [UserDTO!]!user
-'''
+"""
