7a8ae53717668fae3cfd370804c3a648bea769d1
Kristy Gao <gaoxuekristy@gmail.com>
2021-06-06 20:51:56 -0400

    migrate database from postgres to mysql

--- backend/python/app/__init__.py
+++ backend/python/app/__init__.py
@@ -38,14 +38,21 @@ def create_app(config_name):
     app.config["CORS_SUPPORTS_CREDENTIALS"] = True
     CORS(app)
 
+    app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
+    app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
+    app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
+    app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
+
+    '''
     app.config[
         "SQLALCHEMY_DATABASE_URI"
-    ] = "postgres://{username}:{password}@{host}:5432/{db}".format(
-        username=os.getenv("POSTGRES_USER"),
-        password=os.getenv("POSTGRES_PASSWORD"),
+    ] = "mysql://{username}:{password}@{host}:5432/{db}".format(
+        username=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
         host=os.getenv("DB_HOST"),
-        db=os.getenv("POSTGRES_DB"),
+        db=os.getenv("MYSQL_DB"),
     )
+    '''
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     firebase_admin.initialize_app()
