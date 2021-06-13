b773321d0822a52c95ce41b47d329650d8d38676
e-wai <40974372+e-wai@users.noreply.github.com>
2021-06-13 18:37:36 -0400

    Fix bug in content loading for story queries (#25)

--- backend/python/app/services/implementations/story_service.py
+++ backend/python/app/services/implementations/story_service.py
@@ -14,7 +14,9 @@ class StoryService(IStoryService):
     def get_stories(self):
         # Entity is a SQLAlchemy model, we can use convenient methods provided
         # by SQLAlchemy like query.all() to query the data
-        return [result.to_dict() for result in Story.query.all()]
+        return [
+            story.to_dict(include_relationships=True) for story in Story.query.all()
+        ]
 
     def get_story(self, id):
         # get queries by the primary key, which is id for the Story table
@@ -22,7 +24,7 @@ class StoryService(IStoryService):
         if story is None:
             self.logger.error("Invalid id")
             raise Exception("Invalid id")
-        return story.to_dict()
+        return story.to_dict(include_relationships=True)
 
     def create_story(self, story, content):
         # create story
