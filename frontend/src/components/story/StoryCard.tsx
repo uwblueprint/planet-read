ed4ccebb8bda2893cab32a51f124397c02037a64
Jennifer Chen <jenn.chenn93@gmail.com>
2021-06-22 13:00:11 -0400

    add graphql queries for card buttons

--- frontend/src/components/story/StoryCard.tsx
+++ frontend/src/components/story/StoryCard.tsx
@@ -1,27 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import { gql, useMutation } from "@apollo/client";
 import { useHistory } from "react-router-dom";
+import AuthContext from "../../contexts/AuthContext";
 import Tag from "./Tag";
 import PrimaryButton from "./PrimaryButton";
 import SecondaryButton from "./SecondaryButton";
 import "./StoryCard.css";
 
 export type StoryCardProps = {
-  id: number;
+  storyId: number;
+  storyTranslationId?: number;
   title: string;
   description: string;
   youtubeLink: string;
   level: number;
-  isStory: boolean;
+  language: string;
 };
 
 const StoryCard = ({
-  id,
+  storyId,
+  storyTranslationId,
   title,
   description,
   youtubeLink,
   level,
-  isStory,
+  language,
 }: StoryCardProps) => {
+  const { authenticatedUser } = useContext(AuthContext);
+  const history = useHistory();
   const embedLink = (originalYoutubeLink: string): string => {
     /*
     Transforms originalYoutubeLink into embed link appropriate for iframe.
@@ -34,10 +40,85 @@ const StoryCard = ({
 
     return originalYoutubeLink.replace("watch?v=", "embed/");
   };
-  const history = useHistory();
-  const navigateTo = () => history.push("/review/id");
+  const handleError = (errorMessage: string) => {
+    alert(errorMessage);
+  };
+
+  const ASSIGN_REVIEWER = gql`
+    mutation assignUserAsReviewer($storyTranslationId: ID!, $userId: ID!) {
+      assignUserAsReviewer(
+        storyTranslationId: $storyTranslationId
+        userId: $userId
+      ) {
+        ok
+      }
+    }
+  `;
+  type AssignReviewer = { ok: boolean };
+  const [assignUserAsReviewer] = useMutation<{
+    assignUserAsReviewer: AssignReviewer;
+  }>(ASSIGN_REVIEWER);
+  const assignReviewer = async () => {
+    try {
+      const result = await assignUserAsReviewer({
+        variables: { storyTranslationId, userId: +authenticatedUser!!.id },
+      });
+      if (result.data?.assignUserAsReviewer.ok) {
+        history.push(`/review/${storyTranslationId}`);
+      } else {
+        handleError("Unable to assign reviewer.");
+      }
+    } catch (err) {
+      handleError(err ?? "Error occurred, please try again.");
+    }
+  };
+
+  const CREATE_TRANSLATION = gql`
+    mutation createStoryTranslation(
+      $storyTranslationData: CreateStoryTranslationRequestDTO!
+    ) {
+      createStoryTranslation(storyTranslationData: $storyTranslationData) {
+        story {
+          storyId
+          translatorId
+          language
+        }
+      }
+    }
+  `;
+  type CreateTranslation = {
+    story: {
+      storyId: number;
+      translatorId: number;
+      language: string;
+    };
+  };
+  const [createTranslation] = useMutation<{
+    createStoryTranslation: CreateTranslation;
+  }>(CREATE_TRANSLATION);
+  const assignTranslator = async () => {
+    try {
+      const storyTranslationData = {
+        storyId,
+        translatorId: +authenticatedUser!!.id,
+        language,
+      };
+      const result = await createTranslation({
+        variables: { storyTranslationData },
+      });
+      if (result.data?.createStoryTranslation.story.storyId) {
+        history.push(`/translation/${storyId}`);
+      } else {
+        handleError("Unable to assign translator.");
+      }
+    } catch (err) {
+      handleError(err ?? "Error occurred, please try again.");
+    }
+  };
+
+  const previewBook = () => history.push("/preview");
   return (
-    <div id={`story-${id}`} className="story-card">
+    <div id={`story-${storyId}`} className="story-card">
       <iframe
         className="youtube-thumbnail"
         src={embedLink(youtubeLink)}
@@ -58,10 +139,10 @@ const StoryCard = ({
       </div>
       <div className="story-card-actions">
         <PrimaryButton
-          onClick={navigateTo}
-          displayText={isStory ? "translate book" : "review book"}
+          onClick={storyTranslationId ? assignReviewer : assignTranslator}
+          displayText={storyTranslationId ? "review book" : "translate book"}
         />
-        <SecondaryButton onClick={navigateTo} displayText="preview book" />
+        <SecondaryButton onClick={previewBook} displayText="preview book" />
       </div>
     </div>
   );
