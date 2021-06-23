301eed199c121115f6be1d10cc3bef9c64b13d1c
Jennifer Chen <32009013+jennchenn@users.noreply.github.com>
2021-06-23 01:03:44 -0400

    Add translate/review story button to card (#36)
    * add translate book primary and secondary buttons
    
    * add graphql queries for card buttons
    
    * fix css declarations

--- frontend/src/components/story/StoryCard.tsx
+++ frontend/src/components/story/StoryCard.tsx
@@ -1,22 +1,66 @@
-import React from "react";
+import React, { useContext } from "react";
+import { gql, useMutation } from "@apollo/client";
+import { useHistory } from "react-router-dom";
+import AuthContext from "../../contexts/AuthContext";
 import Tag from "./Tag";
+import PrimaryButton from "./PrimaryButton";
+import SecondaryButton from "./SecondaryButton";
 import "./StoryCard.css";
 
+type AssignReviewer = { ok: boolean };
+const ASSIGN_REVIEWER = gql`
+  mutation assignUserAsReviewer($storyTranslationId: ID!, $userId: ID!) {
+    assignUserAsReviewer(
+      storyTranslationId: $storyTranslationId
+      userId: $userId
+    ) {
+      ok
+    }
+  }
+`;
+
+type CreateTranslation = {
+  story: {
+    storyId: number;
+    translatorId: number;
+    language: string;
+  };
+};
+const CREATE_TRANSLATION = gql`
+  mutation createStoryTranslation(
+    $storyTranslationData: CreateStoryTranslationRequestDTO!
+  ) {
+    createStoryTranslation(storyTranslationData: $storyTranslationData) {
+      story {
+        storyId
+        translatorId
+        language
+      }
+    }
+  }
+`;
+
 export type StoryCardProps = {
-  id: number;
+  storyId: number;
+  storyTranslationId?: number;
   title: string;
   description: string;
   youtubeLink: string;
   level: number;
+  language: string;
 };
 
 const StoryCard = ({
-  id,
+  storyId,
+  storyTranslationId,
   title,
   description,
   youtubeLink,
   level,
+  language,
 }: StoryCardProps) => {
+  const { authenticatedUser } = useContext(AuthContext);
+  const history = useHistory();
   const embedLink = (originalYoutubeLink: string): string => {
     /*
     Transforms originalYoutubeLink into embed link appropriate for iframe.
@@ -29,9 +73,54 @@ const StoryCard = ({
 
     return originalYoutubeLink.replace("watch?v=", "embed/");
   };
+  const handleError = (errorMessage: string) => {
+    alert(errorMessage);
+  };
 
+  const [assignUserAsReviewer] = useMutation<{
+    assignUserAsReviewer: AssignReviewer;
+  }>(ASSIGN_REVIEWER);
+  const assignReviewer = async () => {
+    try {
+      const result = await assignUserAsReviewer({
+        variables: { storyTranslationId, userId: +authenticatedUser!!.id },
+      });
+      if (result.data?.assignUserAsReviewer.ok) {
+        history.push(`/review/${storyTranslationId}`);
+      } else {
+        handleError("Unable to assign reviewer.");
+      }
+    } catch (err) {
+      handleError(err ?? "Error occurred, please try again.");
+    }
+  };
+
+  const [createTranslation] = useMutation<{
+    createStoryTranslation: CreateTranslation;
+  }>(CREATE_TRANSLATION);
+  const assignTranslator = async () => {
+    try {
+      const storyTranslationData = {
+        storyId,
+        translatorId: +authenticatedUser!!.id,
+        language,
+      };
+      const result = await createTranslation({
+        variables: { storyTranslationData },
+      });
+      if (result.data?.createStoryTranslation.story.storyId) {
+        history.push(`/translation/${storyId}`);
+      } else {
+        handleError("Unable to assign translator.");
+      }
+    } catch (err) {
+      handleError(err ?? "Error occurred, please try again.");
+    }
+  };
+
+  const previewBook = () => history.push("/preview");
   return (
-    <div id={`story-${id}`} className="story-card">
+    <div id={`story-${storyId}`} className="story-card">
       <iframe
         className="youtube-thumbnail"
         src={embedLink(youtubeLink)}
@@ -43,12 +132,20 @@ const StoryCard = ({
       <div className="story-card-details">
         <div className="story-card-top">
           <p className="story-card-title">{title}</p>
-          <Tag displayText={`level ${level}`} />
-          <Tag displayText="language needed" />
+          <div className="story-card-tags">
+            <Tag displayText={`level ${level}`} />
+            <Tag displayText="language needed" />
+          </div>
         </div>
         <p className="story-card-description">{description}</p>
       </div>
-      <div className="story-card-actions" />
+      <div className="story-card-actions">
+        <PrimaryButton
+          onClick={storyTranslationId ? assignReviewer : assignTranslator}
+          displayText={storyTranslationId ? "review book" : "translate book"}
+        />
+        <SecondaryButton onClick={previewBook} displayText="preview book" />
+      </div>
     </div>
   );
 };
--- frontend/src/components/story/StoryList.tsx
+++ frontend/src/components/story/StoryList.tsx
@@ -8,12 +8,14 @@ export type StoryListProps = {
 const StoryList = ({ stories }: StoryListProps) => {
   const storyCards = stories?.map((story: StoryCardProps) => (
     <StoryCard
-      key={`story-card-key-${story.id}`}
-      id={story.id}
+      key={`story-card-key-${story.storyId}`}
+      storyId={1}
+      storyTranslationId={1}
       title={story.title}
       description={story.description}
       youtubeLink={story.youtubeLink}
       level={story.level}
+      language="HINDI"
     />
   ));
 
