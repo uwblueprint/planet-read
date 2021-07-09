a02015e313badde8a6bcfbc2d47c9cace6c13664
lynn-kim <lynnkim2000@gmail.com>
2021-07-08 22:34:37 -0400

    use env variable for db config

--- backend/python/app/__init__.py
+++ backend/python/app/__init__.py
@@ -43,7 +43,7 @@ def create_app(config_name):
     app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
     app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
     app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
-    app.config["MYSQL_DB"] = "planet-read"
+    app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
 
     app.config[
         "SQLALCHEMY_DATABASE_URI"
