6ed890c1801a8c54959443124a20e623b315427a
Kevin <kevinz999@gmail.com>
2021-07-12 00:22:04 -0400

    Implemented GraphQL Interface for sign in with google

--- backend/python/app/graphql/mutations/auth_mutation.py
+++ backend/python/app/graphql/mutations/auth_mutation.py
@@ -82,6 +82,47 @@ class Login(graphene.Mutation):
             raise Exception(error_message if error_message else str(e))
 
 
+class LoginWithGoogle(graphene.Mutation):
+    class Arguments:
+        tokenId = graphene.String(required=True)
+
+    access_token = graphene.String()
+    refresh_token = graphene.String()
+    id = graphene.Int()
+    first_name = graphene.String()
+    last_name = graphene.String()
+    role = graphene.Field(RoleEnum)
+    email = graphene.String()
+    approved_languages = graphene.String()
+
+    def mutate(root, info, tokenId):
+        try:
+            auth_dto = services["auth"].generate_oauth_token(tokenId)
+
+            access_token = auth_dto.access_token
+            refresh_token = auth_dto.refresh_token
+            id = auth_dto.id
+            first_name = auth_dto.first_name
+            last_name = auth_dto.last_name
+            role = auth_dto.role
+            email = auth_dto.email
+            approved_languages = auth_dto.approved_languages
+
+            return LoginWithGoogle(
+                access_token,
+                refresh_token,
+                id,
+                first_name,
+                last_name,
+                role,
+                email,
+                approved_languages,
+            )
+        except Exception as e:
+            error_message = getattr(e, "message", None)
+            raise Exception(error_message if error_message else str(e))
+
+
 class Logout(graphene.Mutation):
     class Arguments:
         userId = graphene.ID(required=True)
