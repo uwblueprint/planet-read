685d501c71481390809e0f2703bb96caa2fb7995
andrewyguo <andrewguo2001@gmail.com>
2021-08-02 21:43:20 -0700

    Resolved comments :3

--- seed_db.sh
+++ seed_db.sh
@@ -105,8 +105,6 @@ else
                 ('Kevin Lobbies Against Expansion of Social Safety Net', 'He complains higher taxes will prevent him from getting a new Gulfstream G650ER for Christmas', 'https://www.youtube.com/watch?v=t8IK0ZqfxNI&t=27s', 2, '[]');
         "
 
-        echo "Finished generating stories..." 
-
     else 
         # stories 
         docker exec -it planet-read_db_1 mysql -u root -proot -e "USE planet-read; ALTER TABLE stories AUTO_INCREMENT = 1;"
@@ -124,10 +122,10 @@ else
                 ('The Great Gatsby', 'F. Scott Fitzgerald', 'https://www.youtube.com/watch?v=DHyUYg8X31c', 4, '[\"ENGLISH_US\"]'),
                 ('1984', 'George Orwell', 'https://www.youtube.com/watch?v=Da-2h2B4faU&t=4s', 2, '[]');
         "
-        
-        echo "Finished generating stories..." 
 
     fi 
+
+    echo "Finished generating stories..." 
 
     # story_translations
     docker exec -it planet-read_db_1 mysql -u root -proot -e "USE planet-read; ALTER TABLE story_translations AUTO_INCREMENT = 1;"
@@ -139,26 +137,20 @@ else
         VALUES 
             (1, 'GERMAN', 'TRANSLATE', 6),
             (1, 'ENGLISH_UK', 'TRANSLATE', 4),
-
             (2, 'GERMAN', 'TRANSLATE', 2),
             (2, 'POLISH', 'TRANSLATE', 2),
-
             (3, 'MANDARIN', 'TRANSLATE', 5),
             (3, 'ENGLISH_UK', 'TRANSLATE', 4),
-
             (4, 'GERMAN', 'TRANSLATE', 6),
             (4, 'ENGLISH_UK', 'TRANSLATE', 4),
-
             (5, 'GERMAN', 'TRANSLATE', 2),
-            (4, 'ENGLISH_UK', 'TRANSLATE', 1),
+            (5, 'ENGLISH_UK', 'TRANSLATE', 1),
             (5, 'PORTUGUESE', 'TRANSLATE', 7),
             (5, 'DUTCH', 'TRANSLATE', 3),
-
             (6, 'ENGLISH_US', 'TRANSLATE', 1);
     "
     echo "Finished generating story translations..." 
     
-
     # Story content 
     docker exec -it planet-read_db_1 mysql -u root -proot -e "USE planet-read; ALTER TABLE story_contents AUTO_INCREMENT = 1;"
 
@@ -169,36 +161,16 @@ else
         INSERT INTO story_contents 
             (story_id, line_index, content) 
         VALUES 
-            ('$STORY_ID', 0, '\"Every two weeks I went to a meeting with them, and in my room here I covered pages with writing. 
-                I bought every known Hebrew dictionary. But the old gentlemen were always ahead of me. 
-                It wasn\'t long before they were ahead of our rabbi; he brought a colleague in. '),
-
-            ('$STORY_ID', 1, 'Mr. Hamilton, you should have sat through some of those nights of argument and discussion. 
-                The questions, the inspection, oh, the lovely thinking—the beautiful thinking.'),
-                                                                                                                                                            
+            ('$STORY_ID', 0, '\"Every two weeks I went to a meeting with them, and in my room here I covered pages with writing. I bought every known Hebrew dictionary. But the old gentlemen were always ahead of me. It wasn\'t long before they were ahead of our rabbi; he brought a colleague in. '),
+            ('$STORY_ID', 1, 'Mr. Hamilton, you should have sat through some of those nights of argument and discussion. The questions, the inspection, oh, the lovely thinking-the beautiful thinking.'),
             ('$STORY_ID', 2, 'After two years we felt that we could approach your sixteen verses of the fourth chapter of Genesis. '),
-
-            ('$STORY_ID', 3, 'My old gentlemen felt that these words were very important too—\'Thou shalt\' and \'Do thou.\' 
-                And this was the gold from our mining: \'Thou mayest.\' \'Thou mayest rule over sin.\' '),
-
-            ('$STORY_ID', 4, 'The old gentlemen smiled and nodded and felt the years were well spent. It brought them out of their
-                Chinese shells too, and right now they are studying Greek.\" '),
-            
-            ('$STORY_ID', 5, 'Samuel said, \"It\'s a fantastic story. And I\'ve tried to follow and maybe I\'ve missed somewhere. 
-                Why is this word so important?\"'),
-            
-            ('$STORY_ID', 6, 'Lee\'s hand shook as he filled the delicate cups. 
-                He drank his down in one gulp. \"Don\'t you see?\" he cried. '),
-            
+            ('$STORY_ID', 3, 'My old gentlemen felt that these words were very important too-\'Thou shalt\' and \'Do thou.\' And this was the gold from our mining: \'Thou mayest.\' \'Thou mayest rule over sin.\' '),
+            ('$STORY_ID', 4, 'The old gentlemen smiled and nodded and felt the years were well spent. It brought them out of their Chinese shells too, and right now they are studying Greek.\" '),
+            ('$STORY_ID', 5, 'Samuel said, \"It\'s a fantastic story. And I\'ve tried to follow and maybe I\'ve missed somewhere. Why is this word so important?\"'),
+            ('$STORY_ID', 6, 'Lee\'s hand shook as he filled the delicate cups. He drank his down in one gulp. \"Don\'t you see?\" he cried. '),
             ('$STORY_ID', 7, '\"The American Standard translation orders men to triumph over sin, and you can call sin ignorance. '),
-            
-            ('$STORY_ID', 8, 'The King James translation makes a promise in \'Thou shalt,\' 
-                meaning that men will surely triumph over sin. '),
-            
-            ('$STORY_ID', 9, 'But the Hebrew word, the word timshel—\'Thou mayest\'—that gives a choice. 
-                It might be the most important word in the world. 
-                That says the way is open. That throws it right back on a man.
-                For if \'Thou mayest\'—it is also true that \'Thou mayest not.\' Don\'t you see?\"');
+            ('$STORY_ID', 8, 'The King James translation makes a promise in \'Thou shalt,\' meaning that men will surely triumph over sin. '),
+            ('$STORY_ID', 9, 'But the Hebrew word, the word timshel-\'Thou mayest\'-that gives a choice. It might be the most important word in the world. That says the way is open. That throws it right back on a man. For if \'Thou mayest\'-it is also true that \'Thou mayest not.\' Don\'t you see?\"');
         "
     done 
     
@@ -207,7 +179,6 @@ else
     # Story translation content 
     docker exec -it planet-read_db_1 mysql -u root -proot -e "USE planet-read; ALTER TABLE story_translation_contents AUTO_INCREMENT = 1;"
 
-
     for STORY_TRANSLATION_ID in 2 3 5 7 11 13 
     do 
         docker exec -it planet-read_db_1 mysql -u root -proot -e "
@@ -215,37 +186,16 @@ else
         INSERT INTO story_translation_contents 
             (story_translation_id, line_index, translation_content) 
         VALUES 
-            ('$STORY_TRANSLATION_ID', 0, '\"Every two weeks I went to a meeting with them, and in my room here I covered pages with writing. 
-                I bought every known Hebrew dictionary. But the old gentlemen were always ahead of me. 
-                It wasn\'t long before they were ahead of our rabbi; he brought a colleague in. '),
-
-            ('$STORY_TRANSLATION_ID', 1, 'Mr. Hamilton, you should have sat through some of those nights of argument and discussion. 
-                The questions, the inspection, oh, the lovely thinking—the beautiful thinking.'),
-                                                                                                                                                            
+            ('$STORY_TRANSLATION_ID', 0, '\"Every two weeks I went to a meeting with them, and in my room here I covered pages with writing. I bought every known Hebrew dictionary. But the old gentlemen were always ahead of me. It wasn\'t long before they were ahead of our rabbi; he brought a colleague in. '),
+            ('$STORY_TRANSLATION_ID', 1, 'Mr. Hamilton, you should have sat through some of those nights of argument and discussion. The questions, the inspection, oh, the lovely thinking-the beautiful thinking.'),
             ('$STORY_TRANSLATION_ID', 2, 'After two years we felt that we could approach your sixteen verses of the fourth chapter of Genesis. '),
-
-            ('$STORY_TRANSLATION_ID', 3, 'My old gentlemen felt that these words were very important too—\'Thou shalt\' and \'Do thou.\' 
-                And this was the gold from our mining: \'Thou mayest.\' \'Thou mayest rule over sin.\' '),
-
-            ('$STORY_TRANSLATION_ID', 4, 'The old gentlemen smiled and nodded and felt the years were well spent. It brought them out of their
-                Chinese shells too, and right now they are studying Greek.\" '),
-            
-            ('$STORY_TRANSLATION_ID', 5, 'Samuel said, \"It\'s a fantastic story. And I\'ve tried to follow and maybe I\'ve missed somewhere. 
-                Why is this word so important?\"'),
-            
-            ('$STORY_TRANSLATION_ID', 6, 'Lee\'s hand shook as he filled the delicate cups. 
-                He drank his down in one gulp. \"Don\'t you see?\" he cried. '),
-            
-            ('$STORY_TRANSLATION_ID', 7, '\"The American Standard translation orders men to triumph over sin, 
-                and you can call sin ignorance. '),
-            
-            ('$STORY_TRANSLATION_ID', 8, 'The King James translation makes a promise in \'Thou shalt,\' 
-                meaning that men will surely triumph over sin. '),
-            
-            ('$STORY_TRANSLATION_ID', 9, 'But the Hebrew word, the word timshel—\'Thou mayest\'—that gives a choice. 
-                It might be the most important word in the world. 
-                That says the way is open. That throws it right back on a man.
-                For if \'Thou mayest\'—it is also true that \'Thou mayest not.\' Don\'t you see?\"');
+            ('$STORY_TRANSLATION_ID', 3, 'My old gentlemen felt that these words were very important too-\'Thou shalt\' and \'Do thou.\' And this was the gold from our mining: \'Thou mayest.\' \'Thou mayest rule over sin.\' '),
+            ('$STORY_TRANSLATION_ID', 4, 'The old gentlemen smiled and nodded and felt the years were well spent. It brought them out of their Chinese shells too, and right now they are studying Greek.\" '),
+            ('$STORY_TRANSLATION_ID', 5, 'Samuel said, \"It\'s a fantastic story. And I\'ve tried to follow and maybe I\'ve missed somewhere. Why is this word so important?\"'),
+            ('$STORY_TRANSLATION_ID', 6, 'Lee\'s hand shook as he filled the delicate cups. He drank his down in one gulp. \"Don\'t you see?\" he cried. '),
+            ('$STORY_TRANSLATION_ID', 7, '\"The American Standard translation orders men to triumph over sin, and you can call sin ignorance. '),
+            ('$STORY_TRANSLATION_ID', 8, 'The King James translation makes a promise in \'Thou shalt,\' meaning that men will surely triumph over sin. '),
+            ('$STORY_TRANSLATION_ID', 9, 'But the Hebrew word, the word timshel-\'Thou mayest\'-that gives a choice. It might be the most important word in the world. That says the way is open. That throws it right back on a man. For if \'Thou mayest\'-it is also true that \'Thou mayest not.\' Don\'t you see?\"');
         "
     done 
 
@@ -286,10 +236,7 @@ else
                 ('$STORY_TRANSLATION_ID', 6, ''),
                 ('$STORY_TRANSLATION_ID', 7, ''),
                 ('$STORY_TRANSLATION_ID', 8, ''),
-                ('$STORY_TRANSLATION_ID', 9, 'But the Hebrew word, the word timshel—\'Thou mayest\'—that gives a choice. 
-                            It might be the most important word in the world. 
-                            That says the way is open. That throws it right back on a man.
-                            For if \'Thou mayest\'—it is also true that \'Thou mayest not.\' Don\'t you see?\"');
+                ('$STORY_TRANSLATION_ID', 9, 'But the Hebrew word, the word timshel-\'Thou mayest\'-that gives a choice. It might be the most important word in the world. That says the way is open. That throws it right back on a man. For if \'Thou mayest\'-it is also true that \'Thou mayest not.\' Don\'t you see?\"');
         "
     done 
 
