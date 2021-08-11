581f42112ffbeab97914fe709b5ac11416cf1853
andrew guo <54864799+andrewyguo@users.noreply.github.com>
2021-08-10 19:40:48 -0700

    Frontend/create preview book model (#57)
    * Rebased all previous commits onto main after Chakra UI Merge
    
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
    
    Got rid of some extra comments
    
    Modal component added, need to do more styling
    
    Need to style text and tweak the spacing a bit more
    
    Added styling
    
    Changed styling, moved button to top right corner
    
    Changed styling so that title does not cross over into space occupied by the button
    
    I literally do not know what I am doing
    
    removed PreviewModal.css as it is no longer needed
    
    * Make the modal, in Kristy's own words, thiccer.
    
    * Made the padding a bit smaller
    
    Implemented Undo/Redo Functionality (#55)
    
    * Implemented Undo/Redo Functionality
    
    * Removed a console.log
    
    * Addressed PR Comments
    
    * Removed another console.log
    
    Added some more styling changes
    
    Make some final styling changes and removed unneccessary props
    
    * I really should have gone into marine biology...
    
    * Flex on the haters
    
    * ajaskldfjaskl;df
    
    Co-authored-by: Kristy Gao <gaoxuekristy@gmail.com>
    
    :3

---
+++ frontend/src/components/homepage/PreviewModal.tsx
@@ -0,0 +1,137 @@
+import React, { useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+import {
+  Badge,
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  Text,
+} from "@chakra-ui/react";
+import convertLanguageTitleCase from "../../utils/LanguageUtils";
+
+export type PreviewModalProps = {
+  storyId: number;
+  title: string;
+  youtubeLink: string;
+  level: number;
+  language: string;
+  previewBook: () => void;
+  preview: boolean;
+  primaryBtnText: string;
+  primaryBtnOnClick: () => any;
+};
+
+const PreviewModal = ({
+  storyId,
+  title,
+  youtubeLink,
+  level,
+  language,
+  previewBook,
+  preview,
+  primaryBtnText,
+  primaryBtnOnClick,
+}: PreviewModalProps) => {
+  const [content, setContent] = useState<string[]>([]);
+
+  const GET_STORY_CONTENTS = (id: number) =>
+    gql`
+      query {
+        storyById(
+          id: ${id}
+        ) {
+          contents {
+            id
+            lineIndex
+            content
+          }
+        }
+      }
+    `;
+
+  useQuery(GET_STORY_CONTENTS(storyId), {
+    fetchPolicy: "cache-and-network",
+    onCompleted: (data) => {
+      const storyContentsArray = data.storyById.contents.map((story: any) => {
+        return story.content;
+      });
+      setContent(storyContentsArray);
+    },
+  });
+
+  const storyContents = content.map((c: string, index: number) => (
+    <Flex padding={3}>
+      <Box
+        width="30px"
+        text-align="center"
+        float="left"
+        margin="0px 10px 0px 0px"
+        color="black"
+      >
+        <Text fontSize="16px" as="b" align="center">
+          {index + 1}
+        </Text>
+      </Box>
+      <Box
+        backgroundColor="blue.50"
+        width="100%"
+        borderRadius="10px"
+        float="right"
+        padding={3}
+        color="black"
+        fontSize="16px"
+        as="b"
+      >
+        {c}
+      </Box>
+    </Flex>
+  ));
+
+  return (
+    <Modal
+      isOpen={preview}
+      onClose={previewBook}
+      motionPreset="slideInBottom"
+      size="4xl"
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalCloseButton position="static" marginLeft="auto" />
+        <ModalHeader paddingTop="-10px">
+          <Heading as="h3" size="md">
+            {title}
+          </Heading>
+          <Box padding="10px 0px 10px 0px" float="left">
+            <Badge>{`Level ${level}`}</Badge>
+            <Badge>{`${convertLanguageTitleCase(language)}`}</Badge>
+          </Box>
+          <Button
+            float="right"
+            width="21%"
+            colorScheme="blue"
+            mr={3}
+            onClick={primaryBtnOnClick()}
+          >
+            {primaryBtnText}
+          </Button>
+        </ModalHeader>
+        <Box display="block" paddingLeft="30px">
+          <Text fontSize="16px" color="grey">
+            <Link href={youtubeLink}>→ Watch the English AniBook</Link>
+          </Text>
+        </Box>
+        <ModalBody>{storyContents}</ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default PreviewModal;
