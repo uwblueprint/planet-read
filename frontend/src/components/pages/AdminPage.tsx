00f85c964f20654267e6f99bd4f76cf786fc30ef
Kristy Gao <gaoxuekristy@gmail.com>
2021-09-12 18:51:56 -0400

    Set up admin & user profile page

---
+++ frontend/src/components/pages/AdminPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminPage = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Admin Page :)</h1>
+    </div>
+  );
+};
+
+export default AdminPage;
