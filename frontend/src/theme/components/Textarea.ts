1a31c0f205a4b20fde743c37bd50c9deed6ac5b8
lynn-kim <lynnkim2000@gmail.com>
2021-10-05 13:18:06 -0700

    alphabetize css properties

--- frontend/src/theme/components/Textarea.ts
+++ frontend/src/theme/components/Textarea.ts
@@ -3,17 +3,17 @@ const Textarea = {
     translationEditable: {
       border: "1px solid",
       borderColor: "gray.300",
+      borderRadius: "3px",
       color: "gray.500",
       fontSize: "12px",
-      borderRadius: "3px",
       padding: "10px",
       margin: "12px",
     },
     maxCharsReached: {
       border: "1px solid red",
       borderRadius: "3px",
-      padding: "10px",
       margin: "12px",
+      padding: "10px",
     },
   },
 };
