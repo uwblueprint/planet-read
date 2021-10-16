a9087665111b20bc7caf77bba90a3ce0c42f93dd
Kristy Gao <gaoxuekristy@gmail.com>
2021-10-16 12:03:59 -0400

    Sort UsersTable by name (#133)

--- frontend/src/components/admin/ManageUsers.tsx
+++ frontend/src/components/admin/ManageUsers.tsx
@@ -1,27 +1,37 @@
 import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { Heading } from "@chakra-ui/react";
-import UsersTable from "./UsersTable";
+import UsersTable, { alphabeticalCompare } from "./UsersTable";
 import { buildUsersQuery, User } from "../../APIClients/queries/UserQueries";
 
-const ManageTranslators = () => {
+export type ManageUsersProps = {
+  isTranslators: boolean;
+};
+
+const ManageUsers = ({ isTranslators }: ManageUsersProps) => {
   const query = buildUsersQuery();
   const [users, setUsers] = useState<User[]>([]);
   useQuery(query.string, {
     fetchPolicy: "cache-and-network",
     onCompleted: (data) => {
-      setUsers(data[query.fieldName]);
+      const newUsers = [...data[query.fieldName]];
+      newUsers.sort(alphabeticalCompare);
+      setUsers(newUsers);
     },
   });
   return (
     <div style={{ textAlign: "center" }}>
       <Heading float="left" margin="20px 30px" size="lg">
-        Manage Translators
+        {`Manage ${isTranslators ? "Translators" : "Reviewers"}`}
       </Heading>
       {/* TODO: add filter component */}
-      <UsersTable isTranslators users={users} />
+      <UsersTable
+        isTranslators={isTranslators}
+        users={users}
+        setUsers={setUsers}
+      />
     </div>
   );
 };
 
-export default ManageTranslators;
+export default ManageUsers;
