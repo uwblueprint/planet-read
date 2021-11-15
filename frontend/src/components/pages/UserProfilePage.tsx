feb88e60f38e97e95757007cfe8b774489fb2fd6
Philips Xu <philips.ilips@gmail.com>
2021-11-14 21:47:24 -0500

    implement base user profile page

--- frontend/src/components/pages/UserProfilePage.tsx
+++ frontend/src/components/pages/UserProfilePage.tsx
@@ -1,18 +1,162 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import { Box } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Text,
+  useStyleConfig,
+} from "@chakra-ui/react";
+import { useQuery, useMutation } from "@apollo/client";
+import Header from "../navigation/Header";
+import {
+  SoftDeleteUserResponse,
+  SOFT_DELETE_USER,
+} from "../../APIClients/mutations/UserMutations";
+import {
+  MANAGE_USERS_TABLE_DELETE_USER_BUTTON,
+  MANAGE_USERS_TABLE_DELETE_USER_CONFIRMATION,
+} from "../../utils/Copy";
+import ConfirmationModal from "../utils/ConfirmationModal";
+import { GET_USER, User } from "../../APIClients/queries/UserQueries";
 
 type UserProfilePageProps = {
-  userId: string | undefined;
+  userId: string;
 };
 
 const UserProfilePage = () => {
   const { userId } = useParams<UserProfilePageProps>();
 
+  const [fullName, setFullName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [role, setRole] = useState<string>("");
+  const [confirmDeleteUser, setConfirmDeleteUser] = useState(false);
+
+  /* eslint-disable */
+  const [approvedLanguagesTranslation, setApprovedLanguagesTranslation] =
+    useState<{ [name: string]: number }>();
+  const [approvedLanguagesReview, setApprovedLanguagesReview] =
+    useState<{ [name: string]: number }>();
+
+  const history = useHistory();
+
+  useQuery(GET_USER(parseInt(userId, 10)), {
+    fetchPolicy: "cache-and-network",
+    onCompleted: (data: { userById: User }) => {
+      const user = data.userById;
+      const translationLanguages = user?.approvedLanguagesTranslation
+        ? JSON.parse(user.approvedLanguagesTranslation.replace(/'/g, '"'))
+        : {};
+      const reviewLanguages = user?.approvedLanguagesReview
+        ? JSON.parse(user.approvedLanguagesReview.replace(/'/g, '"'))
+        : {};
+      setFullName(`${user?.firstName} ${user?.lastName}`);
+      setEmail(user?.email!);
+      setApprovedLanguagesTranslation(translationLanguages);
+      setApprovedLanguagesReview(reviewLanguages);
+      setRole(
+        `Translator ${
+          Object.keys(reviewLanguages).length > 0 ? "& Reviewer" : ""
+        }`,
+      );
+    },
+    onError: () => {
+      history.push("/404");
+    },
+  });
+
+  const [deleteUser] = useMutation<{
+    response: SoftDeleteUserResponse;
+  }>(SOFT_DELETE_USER);
+
+  const closeModal = () => {
+    setConfirmDeleteUser(false);
+  };
+
+  const openModal = () => {
+    setConfirmDeleteUser(true);
+  };
+
+  const callSoftDeleteUserMutation = async () => {
+    await deleteUser({
+      variables: {
+        id: userId,
+      },
+    });
+    closeModal();
+    history.push("/");
+  };
+
+  const filterStyle = useStyleConfig("Filter");
   return (
-    <Box textAlign="center">
-      <h1>User Profile for {userId} :)</h1>
-    </Box>
+    <Flex direction="column" height="100vh">
+      <Header />
+      <Flex direction="row" flex={1}>
+        <Flex sx={filterStyle} maxWidth="400px">
+          <Heading size="lg">{fullName}</Heading>
+          <Heading size="sm" marginTop="36px">Role(s)</Heading>
+          <Text>{role}</Text>
+          <Heading size="sm" marginTop="36px">Email</Heading>
+          <Text>{email}</Text>
+          <Heading size="sm" marginTop="36px">Date joined</Heading>
+          <Text>TODO</Text>
+          <Heading size="sm" marginTop="36px">Last active on</Heading>
+          <Text>TODO</Text>
+        </Flex>
+        <Flex direction="column" margin="40px">
+          <Heading size="lg" marginTop="40px">
+            Approved Languages and Levels
+          </Heading>
+          <Text>TODO: table here</Text>
+          <Heading size="lg" marginTop="56px">
+            Assigned Story Translations
+          </Heading>
+          <Text>TODO: table here</Text>
+          <Heading size="sm" marginTop="56px">
+            Delete user
+          </Heading>
+          <Text>
+            Permanently delete this user and all data associated with them.
+          </Text>
+          <Button
+            colorScheme="red"
+            margin="10px 0px"
+            width="250px"
+            onClick={() => openModal()}
+          >
+            Delete User
+          </Button>
+        </Flex>
+      </Flex>
+      <Flex
+        alignItems="center"
+        boxShadow="0 0 12px -9px rgba(0, 0, 0, 0.7)"
+        direction="row"
+        height="90px"
+        justify="flex-end"
+        padding="20px 30px"
+      >
+        <Box>
+          <Button colorScheme="blue" variant="outline">
+            Cancel
+          </Button>
+          {/* TODO: make this functional.*/}
+          <Button colorScheme="blue" margin="0 48px 0 24px">
+            Save Changes
+          </Button>
+        </Box>
+      </Flex>
+      {confirmDeleteUser && (
+        <ConfirmationModal
+          confirmation={confirmDeleteUser}
+          onClose={closeModal}
+          onConfirmationClick={callSoftDeleteUserMutation}
+          confirmationMessage={MANAGE_USERS_TABLE_DELETE_USER_CONFIRMATION}
+          buttonMessage={MANAGE_USERS_TABLE_DELETE_USER_BUTTON}
+        />
+      )}
+    </Flex>
   );
 };
 
