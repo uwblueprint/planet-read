4788dd7a8431f52153e0483431fff18907e3a6f2
Kristy Gao <gaoxuekristy@gmail.com>
2021-11-18 22:48:16 -0500

    Prevent Dark Mode in chakraui (#164)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, useColorMode } from "@chakra-ui/react";
 import { ApolloProvider } from "@apollo/client";
 
 import Login from "./components/auth/Login";
@@ -21,6 +21,22 @@ import client from "./APIClients/BaseGQLClient";
 
 import { getLocalStorageObj } from "./utils/LocalStorageUtils";
 
+/*
+ * https://github.com/chakra-ui/chakra-ui/issues/4987#issuecomment-965575708
+ * Light mode shouldn't have to be forced in chakra-ui/react 1.7.1
+ * but it's stil buggy on Kristy's windows 10 with default app mode set to dark
+ */
+function ForceLightMode(props: { children: JSX.Element }) {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  useEffect(() => {
+    if (colorMode === "light") return;
+    toggleColorMode();
+  }, [colorMode]);
+
+  return props.children;
+}
+
 const App = () => {
   const currentUser: AuthenticatedUser = getLocalStorageObj(
     AUTHENTICATED_USER_KEY,
@@ -31,41 +47,43 @@ const App = () => {
 
   return (
     <ChakraProvider theme={customTheme}>
-      <ApolloProvider client={client}>
-        <AuthContext.Provider
-          value={{ authenticatedUser, setAuthenticatedUser }}
-        >
-          <Router>
-            <Switch>
-              <Route exact path="/login" component={Login} />
-              <PrivateRoute exact path="/" component={HomePage} />
-              <PrivateRoute
-                exact
-                path="/user/:userId"
-                component={UserProfilePage}
-              />
-              <PrivateRoute exact path="/admin" component={AdminPage} />
-              <PrivateRoute
-                exact
-                path="/translation/:storyIdParam/:storyTranslationIdParam"
-                component={TranslationPage}
-              />
-              <PrivateRoute
-                exact
-                path="/review/:storyIdParam/:storyTranslationIdParam"
-                component={ReviewPage}
-              />
-              <PrivateRoute
-                exact
-                path="/story/:storyIdParam/:storyTranslationIdParam"
-                component={ManageStoryTranslationPage}
-              />
-              <Route exact path="/404" component={NotFound} />
-              <Route exact path="*" component={NotFound} />
-            </Switch>
-          </Router>
-        </AuthContext.Provider>
-      </ApolloProvider>
+      <ForceLightMode>
+        <ApolloProvider client={client}>
+          <AuthContext.Provider
+            value={{ authenticatedUser, setAuthenticatedUser }}
+          >
+            <Router>
+              <Switch>
+                <Route exact path="/login" component={Login} />
+                <PrivateRoute exact path="/" component={HomePage} />
+                <PrivateRoute
+                  exact
+                  path="/user/:userId"
+                  component={UserProfilePage}
+                />
+                <PrivateRoute exact path="/admin" component={AdminPage} />
+                <PrivateRoute
+                  exact
+                  path="/translation/:storyIdParam/:storyTranslationIdParam"
+                  component={TranslationPage}
+                />
+                <PrivateRoute
+                  exact
+                  path="/review/:storyIdParam/:storyTranslationIdParam"
+                  component={ReviewPage}
+                />
+                <PrivateRoute
+                  exact
+                  path="/story/:storyIdParam/:storyTranslationIdParam"
+                  component={ManageStoryTranslationPage}
+                />
+                <Route exact path="/404" component={NotFound} />
+                <Route exact path="*" component={NotFound} />
+              </Switch>
+            </Router>
+          </AuthContext.Provider>
+        </ApolloProvider>
+      </ForceLightMode>
     </ChakraProvider>
   );
 };
