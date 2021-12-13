ab9c7153c9751aab3480a7a419a2a8a4e0d4933e
lynn-kim <lynnkim2000@gmail.com>
2021-12-13 00:54:53 -0500

    add comments

--- backend/python/app/models/story.py
+++ backend/python/app/models/story.py
@@ -13,6 +13,9 @@ class Story(db.Model):
     description = db.Column(LONGTEXT, nullable=False)
     youtube_link = db.Column(TEXT, nullable=False)
     level = db.Column(db.Integer, nullable=False)
+    # Note: translated_languages should be an enum array, but postgres
+    # has a weird relationship with enums and we're going to switch to
+    # mysql anyways.
     translated_languages = db.Column(db.JSON)
     contents = db.relationship(StoryContent)
     is_test = db.Column(Boolean, default=False, nullable=False)
