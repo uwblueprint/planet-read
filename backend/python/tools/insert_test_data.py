e3132cdb77dee1e5e4462a6056514915c065e6ca
Kristy Gao <gaoxuekristy@gmail.com>
2021-12-13 10:51:05 -0500

    Create new StoryTranslation test

--- backend/python/tools/insert_test_data.py
+++ backend/python/tools/insert_test_data.py
@@ -39,15 +39,18 @@ def insert_test_data():
     db.engine.execute("ALTER TABLE stories AUTO_INCREMENT = 1;")
     db.engine.execute(
         "INSERT IGNORE INTO stories \
-                (id, title, description, youtube_link, level, translated_languages) \
+                (id, title, description, youtube_link, level, translated_languages, is_test) \
             VALUES \
-                (1, 'East of Eden', 'Follow the intertwined destinies of two families whose generations reenact the poisonous rivalry of Cain and Abel.', 'https://www.youtube.com/watch?v=redECmF7wh8', 4, '[\"GERMAN\", \"ENGLISH_UK\"]'), \
-                (2, 'War and Peace', 'War and Peace is a literary work mixed with chapters on history and philosophy by the Russian author Leo Tolstoy.', 'https://www.youtube.com/watch?v=4dn7TEjnbPY', 1, '[\"GERMAN\", \"POLISH\"]'), \
-                (3, 'A Tale of Two Cities', 'An 1859 historical novel by Charles Dickens, set in London and Paris before and during the French Revolution.', 'https://www.youtube.com/watch?v=5czA_L_eOp4', 3, '[\"MANDARIN\", \"ENGLISH_UK\"]'), \
-                (4, 'Pride and Prejudice', 'Pride and Prejudice preaches the difference between superficial goodness and actual goodness.', 'https://www.youtube.com/watch?v=5xTh44G6RYs', 4, '[\"GERMAN\", \"ENGLISH_UK\"]'), \
-                (5, 'To Kill a Mockingbird', 'To Kill a Mockingbird is a novel by the American author Harper Lee. A sentence cannot do this novel justice.', 'https://www.youtube.com/watch?v=3xM8hvEE2dI', 3, '[\"GERMAN\", \"ENGLISH_UK\", \"PORTUGUESE\", \"DUTCH\"]'), \
-                (6, 'The Great Gatsby', 'Set in the Jazz Age on Long Island, near New York City, the novel depicts mysterious millionaire Jay Gatsby and Gatsby and Daisy Buchanan.', 'https://www.youtube.com/watch?v=e6Iu29TNfkM', 4, '[\"ENGLISH_US\"]'), \
-                (7, 'Nineteen Eighty-Four', 'Nineteen Eighty-Four, often referred to as 1984, is a dystopian social science fiction novel by the English novelist George Orwell.', 'https://www.youtube.com/watch?v=h9JIKngJnCU', 2, '[]');"
+                (1, 'East of Eden', 'Follow the intertwined destinies of two families whose generations reenact the poisonous rivalry of Cain and Abel.', 'https://www.youtube.com/watch?v=redECmF7wh8', 4, '[\"GERMAN\", \"ENGLISH_UK\"]', false), \
+                (2, 'War and Peace', 'War and Peace is a literary work mixed with chapters on history and philosophy by the Russian author Leo Tolstoy.', 'https://www.youtube.com/watch?v=4dn7TEjnbPY', 1, '[\"GERMAN\", \"POLISH\"]', false), \
+                (3, 'A Tale of Two Cities', 'An 1859 historical novel by Charles Dickens, set in London and Paris before and during the French Revolution.', 'https://www.youtube.com/watch?v=5czA_L_eOp4', 3, '[\"MANDARIN\", \"ENGLISH_UK\"]', false), \
+                (4, 'Pride and Prejudice', 'Pride and Prejudice preaches the difference between superficial goodness and actual goodness.', 'https://www.youtube.com/watch?v=5xTh44G6RYs', 4, '[\"GERMAN\", \"ENGLISH_UK\"]', false), \
+                (5, 'To Kill a Mockingbird', 'To Kill a Mockingbird is a novel by the American author Harper Lee. A sentence cannot do this novel justice.', 'https://www.youtube.com/watch?v=3xM8hvEE2dI', 3, '[\"GERMAN\", \"ENGLISH_UK\", \"PORTUGUESE\", \"DUTCH\"]', false), \
+                (6, 'The Great Gatsby', 'Set in the Jazz Age on Long Island, near New York City, the novel depicts mysterious millionaire Jay Gatsby and Gatsby and Daisy Buchanan.', 'https://www.youtube.com/watch?v=e6Iu29TNfkM', 4, '[\"ENGLISH_US\"]', false), \
+                (7, 'Nineteen Eighty-Four', 'Nineteen Eighty-Four, often referred to as 1984, is a dystopian social science fiction novel by the English novelist George Orwell.', 'https://www.youtube.com/watch?v=h9JIKngJnCU', 2, '[]', false), \
+                (8, 'The Musical Donkey', 'There was a donkey who felt so happy that he sang through the night in the cucumber field. The problem was that the cucumbers couldn\"t bear it. What did they do?', 'https://www.youtube.com/watch?v=QfcttsaHTIY', 2, '[]', true), \
+                (9, 'The Musical Donkey  Level 3', 'There was a donkey who felt so happy that he sang through the night in the cucumber field. The problem was that the cucumbers couldn\"t bear it. What did they do?', 'https://www.youtube.com/watch?v=QfcttsaHTIY', 3, '[]', true), \
+                (10, 'The River and the Mountain', 'The River thinks that the Mountain\"s life is more comfortable and peaceful than hers. Is it true?', 'https://www.youtube.com/watch?v=DlQ4zvJymKI', 4, '[]', true);"
     )
 
     generic_content = [
@@ -63,6 +66,23 @@ def insert_test_data():
         "But the Hebrew word, the word timshel-'Thou mayest'-that gives a choice. It might be the most important word in the world. That says the way is open. That throws it right back on a man. For if 'Thou mayest'-it is also true that 'Thou mayest not.' Don't you see?",
     ]
 
+    the_musical_content = [
+        "During the day the donkey worked for the washerman. At night he went for long walks with his friend, the jackal. The jackal never listened but the donkey talked and talked until they reached the cucumber field. Then the donkey would open his mouth wide and bite into a fresh, juicy cucumber, cool as the moon.",
+        "Every night, the two ate.  The donkey ate a LOT.  He grew fat with joy. And then one day he began to sing with joy. 'Oh no!' said the jackal and closed his ears.  'Oh yes!' said the donkey and opened his mouth even wider.",
+        "'STOP!' cried the jackal.  'The farmer will hear you!' But the donkey didn't stop singing. 'STOP!' cried the jackal.   'The cucumbers will hear you!'  Still the donkey didn't stop singing. But the farmer didn't hear him. You know why?",
+        "He was dreaming he had a thousand cucumbers in his field.   He didn't want to wake up. In the field, the real cucumbers waited. But the farmer did not come. And the musical donkey would not stop singing.",
+        "The cucumbers could not bear it.   What could they do? One by one, they jumped out of the field and ran to the farmer's house. The next morning, when the farmer woke up, he was not alone. All around him were cucumbers - wide awake, looking at him. And in the field the donkey was fast asleep.",
+    ]
+
+    the_river_and_mountain = [
+        "One day, the river thought to herself, 'Do I have to keep flowing all my life? Can't I stop and rest for a little while?' She needed advice, so she called out to the mountain and shared her thoughts with him. The mountain just laughed and said: 'Hey, look at me, I have been standing in the same place for ages!'",
+        "The river replied, 'You are firm and fixed in one place, how could you ever get tired? Look at me, I have to be on the move all the time. I don't get to rest for even a single minute!'",
+        "The mountain smiled. 'That is how you see it, but I get tired of standing here,' he replied. 'Every day I see the same trees and the same patch of sky. Sometimes I wish: if only I could run around like the river. I would visit new forests and villages. I would water their fields, give life, and be so deeply loved by all.'",
+        "The river interrupted, 'That's strange! Your life is so comfortable and peaceful, yet you feel this way.'",
+        "'You don't get it, my sister. You are worshiped by everyone.' the mountain replied lovingly. 'You flow for the sake of others. And that's not all! After giving away so much, you offer whatever remains, to the sea.'",
+        "On hearing this, the river bowed down to the mountain and said with great enthusiasm, 'You are absolutely right, my brother. The true purpose of my life is to give life to others. Thank you for your kind wisdom!' she said. Then, while the mountain smiled at her newfound positive energy, she gushed away with a loud gurgle, feeling very happy.",
+    ]
+
     # story contents
     db.engine.execute("ALTER TABLE story_contents AUTO_INCREMENT = 1;")
     for story_id in range(1, 8):
@@ -75,6 +95,28 @@ def insert_test_data():
                         ({id}, {story_id}, {i}, "{content}") \
                 '
             )
+
+    for story_id in range(8, 10):
+        for i, content in enumerate(the_musical_content):
+            id = (story_id - 1) * 10 + i + 1
+            db.engine.execute(
+                f'INSERT IGNORE INTO story_contents \
+                        (id, story_id, line_index, content) \
+                    VALUES \
+                        ({id}, {story_id}, {i}, "{content}") \
+                '
+            )
+
+    story_id = 10
+    for i, content in enumerate(the_river_and_mountain):
+        id = (story_id - 1) * 10 + i + 1
+        db.engine.execute(
+            f'INSERT IGNORE INTO story_contents \
+                    (id, story_id, line_index, content) \
+                VALUES \
+                    ({id}, {story_id}, {i}, "{content}") \
+            '
+        )
 
     # story translations
     db.engine.execute("ALTER TABLE story_translations_all AUTO_INCREMENT = 1;")
