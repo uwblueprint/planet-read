6da76bdee0beb86ae69afc1bfe066638b85fe414
Jennifer Lu <jenniferlugm@gmail.com>
2021-12-28 17:37:25 -0500

    Resize translation progress font with font scaler

--- frontend/src/components/utils/ProgressBar.tsx
+++ frontend/src/components/utils/ProgressBar.tsx
@@ -1,25 +1,53 @@
 import React from "react";
-import { Progress } from "@chakra-ui/react";
+import { Progress, Text } from "@chakra-ui/react";
 import "./ProgressBar.css";
 import { truncate } from "../../utils/Utils";
 
 type ProgressBarProps = {
   percentageComplete: number;
   type: string;
+  fontSize: string;
 };
 
-const ProgressBar = ({ percentageComplete, type }: ProgressBarProps) => {
+const ProgressBar = ({
+  percentageComplete,
+  type,
+  fontSize,
+}: ProgressBarProps) => {
+  let progressFontSize;
+  let progressWidth;
+  let progressMargin;
+  switch (fontSize) {
+    case "12px":
+      progressFontSize = "10px";
+      progressWidth = "130px";
+      progressMargin = "10px";
+      break;
+    case "16px":
+      progressFontSize = "12px";
+      progressWidth = "155px";
+      progressMargin = "15px";
+      break;
+    case "24px":
+      progressFontSize = "16px";
+      progressWidth = "210px";
+      progressMargin = "20px";
+      break;
+    default:
+      break;
+  }
+
   return (
     <div className="progress-bar-container">
-      <p className="progress-label">
+      <Text color="gray.400" fontSize={progressFontSize} marginBottom="8px">
         {type} Progress: {truncate(percentageComplete, 2)}%
-      </p>
+      </Text>
       <div>
         <Progress
           value={percentageComplete}
           size="xs"
-          width="130px"
-          marginRight="10px"
+          width={progressWidth}
+          marginRight={progressMargin}
         />
       </div>
     </div>
