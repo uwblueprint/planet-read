a488483e31f7c4e49dd343fb666340f03ce97bde
lynn-kim <lynnkim2000@gmail.com>
2022-01-02 14:27:23 -0500

    Hide resume when NULL

--- frontend/src/components/pages/UserProfilePage.tsx
+++ frontend/src/components/pages/UserProfilePage.tsx
@@ -161,7 +161,7 @@ const UserProfilePage = () => {
             Last active on
           </Heading>
           <Text>TODO</Text>
-          {isAdmin && (
+          {isAdmin && resumeId && (
             <>
               <Heading size="sm" marginTop="36px">
                 User Files
