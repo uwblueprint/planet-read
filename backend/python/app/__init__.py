1c7107746495e1d08c883ea0fd6b12136f74bb7b
Jennifer Chen <jenn.chenn93@gmail.com>
2022-02-26 13:28:49 +0800

    fixup! rollback on any error

--- backend/python/app/__init__.py
+++ backend/python/app/__init__.py
@@ -66,8 +66,6 @@ def create_app(config_name):
     )
 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SQLALCHEMY_POOL_RECYCLE"] = 499
-    app.config["SQLALCHEMY_POOL_TIMEOUT"] = 20
     mysql.init_app(app)
     firebase_admin.initialize_app()
 
